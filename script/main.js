b1d59712ba13d318f5bea682f21d313f7fa3612b
merri2017 <merrishel.richardson@gmail.com>
2018-09-19 02:42:02 -0400

    proper materialize grid, comments and style

--- script/main.js
+++ script/main.js
@@ -1,5 +1,5 @@
 
 // after a set time, page goes to main page    https://appendto.com/2016/04/javascript-redirect-how-to-redirect-a-web-page-with-javascript/
-// setTimeout(function(){ 
-// 	window.location.href = "index.html";
-// 	 }, 4000);
+setTimeout(function(){ 
+	window.location.href = "index.html";
+	 }, 4000);
