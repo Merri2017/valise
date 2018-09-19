1a418d22f5b9d6678b325e710b7540bbfefb0d3b
merri2017 <merrishel.richardson@gmail.com>
2018-09-19 16:07:52 -0400

    modal: It works, now adding elements

---
+++ script/modal.js
@@ -0,0 +1,8 @@
+ 	// This is to open the modal box when button is pressed
+ $(document).ready(function(){
+    	$('.modal').modal();
+    });
+
+ // a function to push elements when called, instead of using seperate page links
+
+ function addModal(image, story, code, site)
