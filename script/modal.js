c890041b2daad3b2fdc10e855600e3bb3d98c519
merri2017 <merrishel.richardson@gmail.com>
2018-09-22 04:16:29 -0400

    added mixin file, links to footer, need to fix JavaScript

--- script/modal.js
+++ script/modal.js
@@ -4,5 +4,40 @@
     });
 
  // a function to push elements when called, instead of using seperate page links
+ 
+ // function addModal(projectName,image, story, code, site){
+ // 	this.name = name;
+ // 	this.languages = languages;
+ // 	this.image = image;
+ // 	this.story = story;
+ // 	this.code = code;
+ // 	this.site = site;
+ // 	document.getElementById('name').value
+ // }
 
- function addModal(image, story, code, site)
+ // var calculator = new addModal('Avdanced calculator', "javascript", "../images/pink.jpg", "This is my advance calculator with a suprise", 'src="https://github.com/Merri2017/B2T-The-Heights-Cohort2/tree/master/advancecalculator"', "src='google.com" );
+
+ // calculator.addModal();
+
+//  name languages inspo PICS
+// .value returns the value of the tag
+
+// WAHH this is hard!!! LOL Im going home
+
+// $('.modal-trigger').click
+ function Project (name, inspo){
+	this.name = name;
+	// this.languages = [];
+	// for (i = 0; i <languages.length;i++){
+	// 	languages.push(languages[i]);
+	// }
+	this.inspo = inspo;
+	// this.github = github;
+	
+};
+
+var project1 = new Project('Advanced Calculator', 'A beautiful calculator with an element of  suprise!');
+
+// document.getElementById('p1').innerHTML= name;
+// 	// document.getElementById('p2').innerHTML= languages;
+	document.getElementById('p3').value= inspo;
